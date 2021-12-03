adf4caea3581e9ab736a418455881c621c09c945
dsychev80 <dsychev80@icloud.com>
2021-12-03 15:08:58 +0300

    move-HourWeatheCell-subviews-to-custom-classes

--- WeatherApp/Screens/MainFragment/HourWeatherCell.swift
+++ WeatherApp/Screens/MainFragment/HourWeatherCell.swift
@@ -9,49 +9,21 @@ import UIKit
 
 class HourWeatherCell: UICollectionViewCell {
     
-    private var backView: UIView = {
-        let view = UIView()
-        view.backgroundColor = Constants.backgroundColor
-        view.layer.cornerRadius = 16
-        view.translatesAutoresizingMaskIntoConstraints = false
-        return view
-    }()
-    
-    private var hourLabel: UILabel = {
-        let label = UILabel()
-        label.font = AppFont.medium.size(16)
-        label.textColor = Constants.hourFontColor
-        label.textAlignment = .center
-        // FIXME: Default data, need to delete after creating model.
-        label.text = "12:00"
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
-    
-    private var weatherImage: UIImageView = {
-        let imageView = UIImageView(image: UIImage(named: "Sun"))
-        
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        return imageView
-    }()
-    
-    private var tempLabel: UILabel = {
-        let label = UILabel()
-        label.font = AppFont.extraBold.size(16)
-        label.textColor = Constants.tempFontColor
-        label.textAlignment = .center
-        // FIXME: Default data, need to delete after creating model.
-        label.text = "25°"
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
+    private var backView = HourBackgroundView(withColor: UIColor(displayP3Red: 234/255,
+                                                                 green: 236/255,
+                                                                 blue: 239/255,
+                                                                 alpha: 1))
+    private var hourLabel = WeatherCellLabel(withFont: AppFont.medium.size(16),
+                                             fontColor: HOUR_FONT_COLOR)
+    private var weatherImage = UIImageView(image: UIImage(named: "Sun"))
+    private var tempLabel = WeatherCellLabel(withFont: AppFont.extraBold.size(16),
+                                             fontColor: TEMP_FONT_COLOR)
     
     // MARK: - Lifecycle
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        setupViewHierarchy()
-        setupLayoutConstraints()
+        setup()
     }
     
     required init?(coder: NSCoder) {
@@ -68,8 +40,16 @@ class HourWeatherCell: UICollectionViewCell {
     
     override func prepareForReuse() {
         hourLabel.text = ":"
-        
         tempLabel.text = "-"
+    }
+    
+    private func setup() {
+        hourLabel.textAlignment = .center
+        tempLabel.textAlignment = .center
+        weatherImage.translatesAutoresizingMaskIntoConstraints = false
+        
+        setupViewHierarchy()
+        setupLayoutConstraints()
     }
     
     private func setupViewHierarchy() {
@@ -80,8 +60,8 @@ class HourWeatherCell: UICollectionViewCell {
     }
     
     private func setupLayoutConstraints() {
-        backView.heightAnchor.constraint(equalToConstant: Content_Cell_Height).isActive = true
-        backView.widthAnchor.constraint(equalToConstant: Content_Cell_Width).isActive = true
+        backView.heightAnchor.constraint(equalToConstant: CONTENT_CELL_HEIGHT).isActive = true
+        backView.widthAnchor.constraint(equalToConstant: CONTENT_CELL_WIDTH).isActive = true
         
         
         hourLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
@@ -90,25 +70,29 @@ class HourWeatherCell: UICollectionViewCell {
         weatherImage.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
         weatherImage.centerYAnchor.constraint(equalTo: contentView.centerYAnchor).isActive = true
         
-        weatherImage.heightAnchor.constraint(equalToConstant: Image_Height).isActive = true
-        weatherImage.widthAnchor.constraint(equalToConstant: Image_Width).isActive = true
+        weatherImage.heightAnchor.constraint(equalToConstant: IMAGE_HEIGHT).isActive = true
+        weatherImage.widthAnchor.constraint(equalToConstant: IMAGE_WIDTH).isActive = true
         
         tempLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
         tempLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -12).isActive = true
     }
     
     // MARK: - Constants
-    struct Constants {
-        static let backgroundColor = UIColor(displayP3Red: 234/255, green: 236/255, blue: 239/255, alpha: 1)
-        static let hourFontColor = UIColor(displayP3Red: 143/255, green: 150/255, blue: 161/255, alpha: 1)
-        static let tempFontColor = UIColor(displayP3Red: 42/255, green: 45/255, blue: 51/255, alpha: 1)
-    }
+
+    private static let HOUR_FONT_COLOR = UIColor(displayP3Red: 143/255,
+                                                 green: 150/255,
+                                                 blue: 161/255,
+                                                 alpha: 1)
+    private static let TEMP_FONT_COLOR = UIColor(displayP3Red: 42/255,
+                                                 green: 45/255,
+                                                 blue: 51/255,
+                                                 alpha: 1)
     
-    private let Content_Cell_Height: CGFloat = 114
-    private let Content_Cell_Width: CGFloat = 73
-    private let Temp_Label_Gap: CGFloat = 12
-    private let Image_Width: CGFloat = 26
-    private let Image_Height: CGFloat = 32
+    private let CONTENT_CELL_HEIGHT: CGFloat = 114
+    private let CONTENT_CELL_WIDTH: CGFloat = 73
+    private let TEMP_LABEL_GAP: CGFloat = 12
+    private let IMAGE_WIDTH: CGFloat = 26
+    private let IMAGE_HEIGHT: CGFloat = 32
     
 }
 
