df76b1253a5b84932a6d49eab51c0e07e5cc5bd5
dsychev80 <dsychev80@icloud.com>
2021-12-06 09:45:50 +0300

    Rename files

---
+++ WeatherApp/UI/CitySearch/BackView.swift
@@ -0,0 +1,20 @@
+//
+//  BackView.swift
+//  WeatherApp
+//
+//  Created by Denis Sychev on 06.12.2021.
+//
+
+import UIKit
+
+class BackView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
