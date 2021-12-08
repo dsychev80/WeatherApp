8a7b7bcb91e6cc3edb9bbff5a684ebca150fb23a
dsychev80 <dsychev80@icloud.com>
2021-12-08 10:24:53 +0300

    Solve issue with launching the app in iOS simulator while do unit testing.

--- WeatherApp/AppDelegate.swift
+++ WeatherApp/AppDelegate.swift
@@ -13,7 +13,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        // cancels further launching of the app if testing
+        if ProcessInfo.processInfo.environment["XCInjectBundleInto"] != nil {
+            return false
+        }
+        
         return true
     }
 
