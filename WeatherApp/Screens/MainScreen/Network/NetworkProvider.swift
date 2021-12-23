9d369a28ced007802c3ced85ee3af7c6acdedeef
dsychev80 <dsychev80@icloud.com>
2021-12-23 14:45:38 +0300

    Implement Alamofire

--- WeatherApp/Screens/MainScreen/Network/NetworkProvider.swift
+++ WeatherApp/Screens/MainScreen/Network/NetworkProvider.swift
@@ -5,35 +5,27 @@
 //  Created by Denis Sychev on 28.11.2021.
 //
 
-import Foundation
+import Alamofire
 
 
 final class NetworkProvider: NetworkManager {
     
     public func loadWeatherForLocation(_ location: LocationData, completion: @escaping (Result<JSONWeatherData, WeatherError>) -> Void) {
-        
+
         var api = WeatherResource()
         api.addLocation(location)
         guard let url = api.url else {
             print("guard condition not met at: \(#file) \(#line) \(#function)")
             return
         }
-        
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            guard error == nil, let data = data else {
-                print("guard condition not met at: \(#file) \(#line) \(#function)")
-                completion(.failure(WeatherError.serverError(error?.localizedDescription ?? "Unrecognized network error")))
-                return
-            }
-            
-            do {
-                let jsonDecoder = JSONDecoder()
-                let jsonData = try jsonDecoder.decode(JSONWeatherData.self, from: data)
-                completion(.success(jsonData))
-            } catch {
-                completion(.failure(WeatherError.decodingError(error.localizedDescription)))
-            }
 
-        }.resume()
+        AF.request(url).validate().responseDecodable(of: JSONWeatherData.self) { response in
+            switch response.result {
+            case .success(let data):
+                completion(.success(data))
+            case .failure(let error):
+                completion(.failure(WeatherError.errorFromLocationServer(error.localizedDescription)))
+            }
+        }
     }
 }
