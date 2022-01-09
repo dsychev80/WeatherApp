1a12d670f98bcd8677b024d130ff5004b524da22
dsychev80 <dsychev80@icloud.com>
2022-01-09 13:17:40 +0400

    Correct some errors after merge conflict

--- WeatherApp/Screens/MainScreen/Network/ResponseCacher.swift
+++ WeatherApp/Screens/MainScreen/Network/ResponseCacher.swift
@@ -2,14 +2,14 @@
 //  ResponseCacher.swift
 //  WeatherApp
 //
-//  Created by Denis Sychev on 08.01.2022.
+//  Created by Denis Sychev on 09.01.2022.
 //
 
 import Alamofire
 
 fileprivate let CACHE_FRESHNESS_VALUE: Double = 60 * 60
 
-final class ResponseCacher: CachedResponseHandler {
+final class ResponseCacher {
     private var cachedUrl: URL?
     private var cachedData: CachedURLResponse?
     private var lastUpdateDate: Date!
@@ -21,6 +21,9 @@ final class ResponseCacher: CachedResponseHandler {
     private func isCachedDataFresh() -> Bool {
         return abs(lastUpdateDate.timeIntervalSinceNow) < CACHE_FRESHNESS_VALUE
     }
+}
+
+extension ResponseCacher: CachedResponseHandler {
     
     public func dataTask(_ task: URLSessionDataTask, willCacheResponse response: CachedURLResponse, completion: @escaping (CachedURLResponse?) -> Void) {
         if let url = task.currentRequest?.url {
