29af3ddcba9c2ca32da50f85334b1174d3fad398
dsychev80 <dsychev80@icloud.com>
2022-01-10 13:52:53 +0400

    Add Needle to project

--- Pods/Target Support Files/Pods-WeatherApp/Pods-WeatherApp-frameworks.sh
+++ Pods/Target Support Files/Pods-WeatherApp/Pods-WeatherApp-frameworks.sh
@@ -177,11 +177,13 @@ code_sign_if_enabled() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/NeedleFoundation/NeedleFoundation.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/R.swift.Library/Rswift.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SnapKit/SnapKit.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/NeedleFoundation/NeedleFoundation.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/R.swift.Library/Rswift.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SnapKit/SnapKit.framework"
 fi
